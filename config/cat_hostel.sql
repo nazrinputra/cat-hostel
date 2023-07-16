c1ec60ec9b72115d3de948f301bf519cf97d0e90
nazrinputra <putra_naz94@yahoo.com>
2023-07-16 12:18:02 +0800

    Update to latest DB

--- config/cat_hostel.sql
+++ config/cat_hostel.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 16, 2023 at 01:41 AM
--- Server version: 8.0.32
--- PHP Version: 8.2.8
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -17,16 +9,8 @@ SET time_zone = "+00:00";
 ;
 /*!40101 SET NAMES utf8mb4 */
 ;
---
--- Database: `cat_hostel`
---
 CREATE DATABASE IF NOT EXISTS `cat_hostel`;
 USE `cat_hostel`;
--- --------------------------------------------------------
---
--- Table structure for table `Booking`
---
-
 DROP TABLE IF EXISTS `Booking`;
 CREATE TABLE `Booking` (
   `booking_id` int NOT NULL,
@@ -36,26 +20,7 @@ CREATE TABLE `Booking` (
   `check_in` date NOT NULL,
   `check_out` date NOT NULL
 ) ENGINE = InnoDB;
---
--- Dumping data for table `Booking`
---
-
-INSERT INTO `Booking` (
-    `booking_id`,
-    `room_id`,
-    `cat_id`,
-    `booking_reference`,
-    `check_in`,
-    `check_out`
-  )
-VALUES (4, 3, 2, '0716-LCMM', '2023-07-14', '2023-07-16'),
-  (5, 2, 2, '0716-HUUV', '2023-07-30', '2023-07-31'),
-  (6, 2, 1, '0716-FBXN', '2023-07-16', '2023-07-16');
--- --------------------------------------------------------
---
--- Table structure for table `Cat`
---
-
+TRUNCATE TABLE `Booking`;
 DROP TABLE IF EXISTS `Cat`;
 CREATE TABLE `Cat` (
   `cat_id` int NOT NULL,
@@ -65,43 +30,14 @@ CREATE TABLE `Cat` (
   `cat_color` varchar(15) NOT NULL,
   `cat_weight` int NOT NULL
 ) ENGINE = InnoDB;
---
--- Dumping data for table `Cat`
---
-
-INSERT INTO `Cat` (
-    `cat_id`,
-    `user_id`,
-    `cat_name`,
-    `cat_gender`,
-    `cat_color`,
-    `cat_weight`
-  )
-VALUES (1, 1, 'Meow', 'Female', 'Orange', 3),
-  (2, 2, 'Cat', 'Male', 'White', 5);
--- --------------------------------------------------------
---
--- Table structure for table `Room`
---
-
+TRUNCATE TABLE `Cat`;
 DROP TABLE IF EXISTS `Room`;
 CREATE TABLE `Room` (
   `room_id` int NOT NULL,
   `room_name` varchar(50) NOT NULL,
   `room_description` varchar(255) DEFAULT NULL
 ) ENGINE = InnoDB;
---
--- Dumping data for table `Room`
---
-
-INSERT INTO `Room` (`room_id`, `room_name`, `room_description`)
-VALUES (2, 'New Room', 'New room description'),
-  (3, 'Big Room', 'Big room description');
--- --------------------------------------------------------
---
--- Table structure for table `User`
---
-
+TRUNCATE TABLE `Room`;
 DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
   `user_id` int NOT NULL,
@@ -110,12 +46,10 @@ CREATE TABLE `User` (
   `user_contact` varchar(15) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `user_password` varchar(50) NOT NULL,
-  `user_role` varchar(10) NOT NULL
+  `user_role` varchar(10) NOT NULL,
+  `user_active` tinyint(1) NOT NULL
 ) ENGINE = InnoDB;
---
--- Dumping data for table `User`
---
-
+TRUNCATE TABLE `User`;
 INSERT INTO `User` (
     `user_id`,
     `user_name`,
@@ -123,7 +57,8 @@ INSERT INTO `User` (
     `user_contact`,
     `user_email`,
     `user_password`,
-    `user_role`
+    `user_role`,
+    `user_active`
   )
 VALUES (
     1,
@@ -132,86 +67,42 @@ VALUES (
     '12345',
     'staff@email.com',
     'password',
-    'Staff'
+    'Staff',
+    1
   ),
   (
     2,
     'customer',
-    'Male',
+    'Female',
     '12345',
-    'customer@email.com',
+    'customer2@email.com',
     'password',
-    'Customer'
+    'Customer',
+    0
   );
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Booking`
---
 ALTER TABLE `Booking`
 ADD PRIMARY KEY (`booking_id`),
   ADD KEY `booking_cat` (`cat_id`),
   ADD KEY `booking_room` (`room_id`);
---
--- Indexes for table `Cat`
---
 ALTER TABLE `Cat`
 ADD PRIMARY KEY (`cat_id`),
   ADD KEY `cat_owner` (`user_id`);
---
--- Indexes for table `Room`
---
 ALTER TABLE `Room`
 ADD PRIMARY KEY (`room_id`);
---
--- Indexes for table `User`
---
 ALTER TABLE `User`
 ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `user_email` (`user_email`);
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Booking`
---
 ALTER TABLE `Booking`
-MODIFY `booking_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 7;
---
--- AUTO_INCREMENT for table `Cat`
---
+MODIFY `booking_id` int NOT NULL AUTO_INCREMENT;
 ALTER TABLE `Cat`
-MODIFY `cat_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 6;
---
--- AUTO_INCREMENT for table `Room`
---
+MODIFY `cat_id` int NOT NULL AUTO_INCREMENT;
 ALTER TABLE `Room`
-MODIFY `room_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 6;
---
--- AUTO_INCREMENT for table `User`
---
+MODIFY `room_id` int NOT NULL AUTO_INCREMENT;
 ALTER TABLE `User`
-MODIFY `user_id` int NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 3;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Booking`
---
+MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
 ALTER TABLE `Booking`
 ADD CONSTRAINT `booking_cat` FOREIGN KEY (`cat_id`) REFERENCES `Cat` (`cat_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `booking_room` FOREIGN KEY (`room_id`) REFERENCES `Room` (`room_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
---
--- Constraints for table `Cat`
---
 ALTER TABLE `Cat`
 ADD CONSTRAINT `cat_owner` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
